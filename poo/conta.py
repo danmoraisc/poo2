057ab6e3fe97f217afc82bf1e970f0e7c700ca7c
DANIEL MORAIS CARDOZO <84000099+danmoraisc@users.noreply.github.com>
2023-07-19 15:23:54 -0400

    Update conta.py

--- poo/conta.py
+++ poo/conta.py
@@ -91,20 +91,3 @@ if __name__ == '__main__':
     # conta1.transferir(10, conta2)
     # conta2.extrato()
 
-"""
-Falamos nessa aula sobre a coesão que é ligado ao principio de responsabilidade única. Aprendemos que uma classe deve ter 
-apenas uma responsabilidade (ou deve ter apenas uma razão para existir). Em outras palavras, ela não deve assumir responsabilidades 
-que não são delas.
-
-Além desse princípio de responsabilidade única existem outras que foram definidos através do Robert C. Martin no início dos anos 2000 
-e são conhecidos pelo acrônimo SOLID:
-
-S - Single responsibility principle
-O - Open/closed principle
-L - Liskov substitution principle
-I - Interface segregation principle
-D - Dependency inversion principle
-
-Na Alura temos cursos específicos sobre o SOLID, mas fique tranquilo, na medida que você avança no mundo OO esses princípios 
-ficam mais claros e fáceis de se entender.
-"""
